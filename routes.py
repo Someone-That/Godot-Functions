fd026f9efda0ba338eb21968a395a23c95cf1293
Mikaeel <20017@burnside.school.nz>
2023-07-18 09:24:40 +1200

    Data display initialization

--- routes.py
+++ routes.py
@@ -5,17 +5,31 @@ import sqlite3
 app = Flask(__name__)
 
 
+conn = sqlite3.connect('functions.db')
+cur = conn.cursor()
+
+
+def sql_statement(sql, table):
+	table = "table"
+	sql = f"SELECT * FROM {table}"
+	cur.execute(sql) #selects all the items from the chosen table
+	return cur.fetchall() #returns the results
+
+
 @app.route('/')
 def home():
-	return render_template("home.html", title = "Home")
+	conn = sqlite3.connect('functions.db')
+	cur = conn.cursor()
+	table = "table"
+	sql = f"SELECT * FROM {table}"
+	cur.execute(sql) #selects all the items from the chosen table
+	results = cur.fetchall() #stores the results in the results variable
+	return render_template("home.html", title = "Home", results=results)
 
 
 @app.route('/add-your-own')
 def add_your_own():
     return render_template("add_your_own.html", title = "Add your own")
-
-
-
 # @app.route('/')
 # def all_pizzas():
 # 	conn = sqlite3.connect('functions.db')
@@ -25,7 +39,6 @@ def add_your_own():
 # 	cur.execute(sql) #selects all the items from the chosen table
 # 	results = cur.fetchall() #stores the results in the results variable
 # 	return render_template('home.html',results=results)
-
 
 
 if __name__ == "__main__":
