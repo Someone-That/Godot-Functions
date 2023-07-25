c5881ab1b2bae7af4f0d438d776082dbea32922f
Mikaeel <20017@burnside.school.nz>
2023-07-26 10:25:33 +1200

    Creating quality of life functions

--- routes.py
+++ routes.py
@@ -6,32 +6,32 @@ app = Flask(__name__)
 
 
 
-def sql_statement(sql, table):
+def sql_statement(sql):
+	'''Runs the provided sql statement and returns the results.'''
 	conn = sqlite3.connect('functions.db')
 	cur = conn.cursor()
-	#if sql == "show": cur.execute("SELECT * FROM ?",(table,)) #selects all the items from the chosen table
-	if sql == "show": cur.execute(f"SELECT * FROM {table};")
-	return cur.fetchall() #returns the results
+	cur.execute(sql) #selects all the items from the chosen table
+	return clean_up_data(cur.fetchall()) #returns the results
+
+
+def clean_up_data(data): 
+	'''this function converts all tuples with only a single item in a list into non tuples, this removes the need to type something like ids[0][0]'''
+	clean_data = []
+	for i in data:
+		clean_data.append(i[0])
+	return clean_data
 
 
 @app.route('/')
 def home():
-	functions = sql_statement("show", "Functions")
-	return render_template("home.html", title = "Home", functions=functions)
+	function_names = sql_statement("SELECT function FROM Functions")
+	descriptions = sql_statement("SELECT description FROM Functions")
+	return render_template("home.html", title = "Home", functions=function_names)
 
 
 @app.route('/add-your-own')
 def add_your_own():
     return render_template("add_your_own.html", title = "Add your own")
-# @app.route('/')
-# def all_pizzas():
-# 	conn = sqlite3.connect('functions.db')
-# 	cur = conn.cursor()
-# 	table = "table"
-# 	sql = f"SELECT * FROM {table}"
-# 	cur.execute(sql) #selects all the items from the chosen table
-# 	results = cur.fetchall() #stores the results in the results variable
-# 	return render_template('home.html',results=results)
 
 
 if __name__ == "__main__":
