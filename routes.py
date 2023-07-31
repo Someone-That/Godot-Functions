c07221f131076a7c93b3d856d1e81d02f76bf1fc
Mikaeel <20017@burnside.school.nz>
2023-08-01 09:25:46 +1200

    Passing all necessary information in functions table

--- routes.py
+++ routes.py
@@ -26,7 +26,10 @@ def clean_up_data(data):
 def home():
 	function_names = sql_statement("SELECT function FROM Functions")
 	descriptions = sql_statement("SELECT description FROM Functions")
-	return render_template("home.html", title = "Home", functions=function_names)
+	return_types = sql_statement("SELECT name FROM Functions r INNER JOIN DataType c ON r.return_type = c.id") #this statement was found from playing around with this answer on stackoverflow https://stackoverflow.com/questions/68387600/sql-convert-foreign-key-table-into-a-specific-value-from-other-table
+	doc_links = sql_statement("SELECT doc_link FROM Functions")
+	function_quantity = sql_statement("SELECT COUNT(id) FROM Functions")
+	return render_template("home.html", title = "Home", function_names=function_names, descriptions=descriptions, return_types=return_types, doc_links=doc_links, function_quantity=function_quantity[0])
 
 
 @app.route('/add-your-own')
