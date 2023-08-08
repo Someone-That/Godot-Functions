91955e90dea81d1ab51d2a826540395482ef117c
Mikaeel <20017@burnside.school.nz>
2023-08-09 10:25:50 +1200

    Working on displaying parameters next to function name

--- routes.py
+++ routes.py
@@ -24,11 +24,28 @@ def clean_up_data(data):
 
 @app.route('/')
 def home():
+	#extract data from database in neat form
 	function_names = sql_statement("SELECT function FROM Functions")
 	descriptions = sql_statement("SELECT description FROM Functions")
 	return_types = sql_statement("SELECT name FROM Functions r INNER JOIN DataType c ON r.return_type = c.id") #this statement was found from playing around with this answer on stackoverflow https://stackoverflow.com/questions/68387600/sql-convert-foreign-key-table-into-a-specific-value-from-other-table
 	doc_links = sql_statement("SELECT doc_link FROM Functions")
 	function_quantity = sql_statement("SELECT COUNT(id) FROM Functions")
+
+	#gets parameter data by extracting functions that have parameters, parameters of those functions, data type of those parameters and then generates a neat list for easy implementation into html note: the sql statements below were created using the stackoverflow statement above
+	functions_that_have_parameters = sql_statement("SELECT function FROM FunctionParameters r INNER JOIN Functions c ON r.fid = c.id")
+	function_parameters = sql_statement("SELECT name FROM FunctionParameters r INNER JOIN Parameters c ON r.pid = c.id")
+	parameters = sql_statement("SELECT name FROM Parameters")
+	parameter_data_types = sql_statement("SELECT name FROM Parameters r INNER JOIN DataType c ON r.data_type = c.id")
+
+	neat_parameter_list = []
+	for i in range(function_quantity):
+		neat_parameter_list.append("()")
+	
+	for i in functions_that_have_parameters:
+		if i in function_names:
+			neat_parameter_list[function_names.index(i)]
+
+	#pass in all the data
 	return render_template("home.html", title = "Home", function_names=function_names, descriptions=descriptions, return_types=return_types, doc_links=doc_links, function_quantity=function_quantity[0])
 
 
