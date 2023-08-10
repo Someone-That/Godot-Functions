e6bb63f1e5ebef595544794a3e6f7e9880b186f4
Mikaeel <20017@burnside.school.nz>
2023-08-11 09:24:36 +1200

    More parameter looping work

--- routes.py
+++ routes.py
@@ -29,7 +29,7 @@ def home():
 	descriptions = sql_statement("SELECT description FROM Functions")
 	return_types = sql_statement("SELECT name FROM Functions r INNER JOIN DataType c ON r.return_type = c.id") #this statement was found from playing around with this answer on stackoverflow https://stackoverflow.com/questions/68387600/sql-convert-foreign-key-table-into-a-specific-value-from-other-table
 	doc_links = sql_statement("SELECT doc_link FROM Functions")
-	function_quantity = sql_statement("SELECT COUNT(id) FROM Functions")
+	function_quantity = sql_statement("SELECT COUNT(id) FROM Functions")[0]
 
 	#gets parameter data by extracting functions that have parameters, parameters of those functions, data type of those parameters and then generates a neat list for easy implementation into html note: the sql statements below were created using the stackoverflow statement above
 	functions_that_have_parameters = sql_statement("SELECT function FROM FunctionParameters r INNER JOIN Functions c ON r.fid = c.id")
@@ -40,16 +40,19 @@ def home():
 	for i in range(function_quantity):
 		neat_parameter_list.append("(")
 	
-	for i in functions_that_have_parameters:
-		if i in function_names:
-			index = function_names.index(i)
-			neat_parameter_list[index] += f"{parameter_data_types[index]} {function_parameters[index]} "
+	for i in range(len(function_parameters)): #goes through the function parameters, gets the index of the function the parameter belongs to by using the functions_that_have_parameters list and then uses that index to add to the parameter list for that function
+		index = function_names.index(functions_that_have_parameters[i])
+		neat_parameter_list[index] += f"{parameter_data_types[i]} {function_parameters[i]}, "
 	
-	for i in range(function_quantity):
+	#for i in functions_that_have_parameters: #removes the space at the end of every parameter list by deleting the last character of every parameter list that has content
+	#	parameter_in_question = neat_parameter_list[function_names.index[i]]
+	#	neat_parameter_list[function_names.index[i]] = parameter_in_question[0:len(parameter_in_question)-2]
+	
+	for i in range(function_quantity): #adds the closing parenthesis
 		neat_parameter_list[i] += ")"
 
 	#pass in all the data
-	return render_template("home.html", title = "Home", function_names=function_names, descriptions=descriptions, return_types=return_types, doc_links=doc_links, function_quantity=function_quantity[0], neat_parameter_list=neat_parameter_list)
+	return render_template("home.html", title = "Home", function_names=function_names, descriptions=descriptions, return_types=return_types, doc_links=doc_links, function_quantity=function_quantity, neat_parameter_list=neat_parameter_list)
 
 
 @app.route('/add-your-own')
