355fcde4292aeda6cf9569d042e023dc8d253d8a
Mikaeel <20017@burnside.school.nz>
2023-08-18 12:45:48 +1200

    Adding custom datatype/parameter input fields

--- routes.py
+++ routes.py
@@ -65,7 +65,8 @@ def home():
 @app.route('/add-your-own')
 def add_your_own():
 	data_types = sql_statement("SELECT name from DataType")
-	return render_template("add_your_own.html", title="Add your own", data_types=data_types)
+	parameters = sql_statement("SELECT name from Parameters")
+	return render_template("add_your_own.html", title="Add your own", data_types=data_types, parameters=parameters)
 
 
 @app.route('/add-your-own', methods=['POST'])
